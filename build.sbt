c2ae8624aac27b939c5ae9c26e48bf86d82b3b7f
a.gnidash <a.gnidash@intetics.com>
2015-10-30 18:31:54 +0200

    xml comparator update

--- build.sbt
+++ build.sbt
@@ -5,6 +5,7 @@ version := "1.0"
 scalaVersion := "2.11.7"
 
 libraryDependencies ++= Seq(
-  "org.apache.tika" % "tika-core" % "1.10",
+  "com.fasterxml.jackson.core" % "jackson-core" % "2.6.3",
+  "com.fasterxml.jackson.core" % "jackson-databind" % "2.6.3",
   "org.scalatest" % "scalatest_2.11" % "2.2.4" % "test"
 )
--- src/main/scala/comparator/ObjectComparator.scala
+++ src/main/scala/comparator/ObjectComparator.scala
@@ -1,52 +1,84 @@
 package comparator
 
-import java.io.ByteArrayInputStream
-import java.nio.charset.StandardCharsets
+import java.io.StringReader
+import javax.xml.parsers.DocumentBuilderFactory
 
-import org.apache.tika.Tika
+import com.fasterxml.jackson.databind.{JsonNode, ObjectMapper}
+import org.w3c.dom.Document
+import org.xml.sax.InputSource
 
-trait ObjectComparator {
-  def compare(expected: String, actual: String): Unit
+trait ObjectComparator[T] {
+  def compare(expected: T, actual: T): Unit
 }
 
 object ObjectComparator {
 
-  object StringComparator extends ObjectComparator {
+  object StringComparator extends ObjectComparator[String] {
     override def compare(expected: String, actual: String): Unit = {
       if (expected != actual) {
-        val c1 = getContentType(expected)
-        val c2 = getContentType(actual)
+        tryParse(expected, actual) match {
+          case JsonObjects(e, a) =>
+            JsonComparator.compare(e, a)
 
-        if (c1 != c2) {
-          throw new ComparisonError(
-            s"""
-              Expected content type is $c1
-              Actual content type is $c2
-            """)
+          case XMLObjects(e, a) =>
+            XMLComparator.compare(e, a)
+
+          case NotEqualContentTypeObjects => throw new NotEqualError(s"Content is not equal and can't be associate with template")
         }
-
-        c1 match {
-          case "application/json" => JsonComparator.compare(expected, actual)
-          case "application/xml" => XMLComparator.compare(expected, actual)
-          case _ => throw new NotEqualError(s"Content is equal by content type $c1 but not equal by string content")
-        }
-      } 
-    }
-
-    def getContentType(source: String) = {
-      new Tika().detect(new ByteArrayInputStream(source.getBytes(StandardCharsets.UTF_8)))
+      }
     }
   }
 
-  object JsonComparator extends ObjectComparator {
-    override def compare(expected: String, actual: String): Unit = {
+  private def tryParse(expected: String, actual: String): ParseResult = {
+    asJson(expected) match {
+      case Some(e) =>
+        asJson(actual) match {
+          case Some(a) => JsonObjects(e, a)
+          case None => NotEqualContentTypeObjects
+        }
 
+      case None =>
+        asXML(expected) match {
+          case Some(e) =>
+            asXML(actual) match {
+              case Some(a) => XMLObjects(e, a)
+              case None => NotEqualContentTypeObjects
+            }
+          case None => NotEqualContentTypeObjects
+        }
     }
   }
 
-  object XMLComparator extends ObjectComparator {
-    override def compare(expected: String, actual: String): Unit = {
+  def asJson(v:String): Option[JsonNode] = {
+    val mapper = new ObjectMapper()
+    try Some(mapper.readTree(v)) catch {
+      case e: Throwable => None
+    }
+  }
 
+  def asXML(v:String): Option[Document] = {
+    val db = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+    val is = new InputSource(new StringReader(v))
+
+    try Some(db.parse(is)) catch {
+      case e: Throwable => None
+    }
+  }
+
+  sealed trait ParseResult
+  case class JsonObjects(expected: JsonNode, actual: JsonNode) extends ParseResult
+  case class XMLObjects(expected: Document, actual: Document) extends ParseResult
+  object NotEqualContentTypeObjects extends ParseResult
+
+  object JsonComparator extends ObjectComparator[JsonNode] {
+    override def compare(expected: JsonNode, actual: JsonNode): Unit = {
+      //TODO
+    }
+  }
+
+  object XMLComparator extends ObjectComparator[Document] {
+    override def compare(expected: Document, actual: Document): Unit = {
+      //TODO
     }
   }
 
